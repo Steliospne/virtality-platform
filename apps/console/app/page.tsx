4ef618401eb59d0ccf6c802bb446eec5d36f6e46
Stelios Pnevmatikakis <scizolock@gmail.com>
2026-03-08 11:42:04 +0100

    fix(console): Admin session cookies check.

--- apps/console/app/page.tsx
+++ apps/console/app/page.tsx
@@ -1,10 +1,16 @@
 import Dashboard from '@/app/(pages)/user/user-dashboard/dashboard'
 import { cookies } from 'next/headers'
 
+const env = process.env.ENV || 'development'
+
 const HomePage = async () => {
   const cookieStore = await cookies()
 
-  const cookie = cookieStore.get('virtality_admin_session')
+  const cookie = cookieStore.get(
+    env === 'production' || env === 'preview'
+      ? '__Secure-virtality_admin_session'
+      : 'virtality_admin_session_local',
+  )
 
   return <Dashboard isImpersonating={!!cookie} />
 }
